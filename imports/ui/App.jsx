036b81c722d0a007180c091ae582475716e74eec
Ryan Yogan <ryanyogan@gmail.com>
2018-01-26 12:22:21 -0800

    added in boilerplate ui import

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Placeholder</h1>
+    <p>
+      If you see this page, than you aren't using the mobile app thus you cannot
+      do much here...
+    </p>
+  </div>
+);
+
+export default App;
