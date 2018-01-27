04c734a9144d9ef69798c5e45d9f1c9ce09574d1
Ryan Yogan <ryanyogan@gmail.com>
2018-01-26 17:18:31 -0800

    added in a method for checking in / out

--- imports/api/locations/methods.js
+++ imports/api/locations/methods.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Locations } from './locations';
@@ -21,5 +22,44 @@ export const getNearestLocations = new ValidatedMethod({
       },
     };
     return Locations.find(query, { limit: 10 }).fetch();
+  },
+});
+
+export const changeCheckInStatus = new ValidatedMethod({
+  name: 'Locations.changeCheckIn',
+  validate: new SimpleSchema({
+    locationId: { type: String },
+    status: { type: String, allowedValues: ['in', 'out'] },
+  }).validator(),
+
+  run({ locationId, status }) {
+    const location = Locations.findOne({ _id: locationId });
+
+    if (!location) {
+      throw new Meteor.Error(
+        'Locations.changeCheckIn.invalidLocationId',
+        'Must pass a valid location id (_id) to change check-in status.',
+      );
+    }
+
+    if (status === 'in') {
+      Locations.update(
+        { _id: locationId },
+        {
+          $set: {
+            checkedInUserId: 'temp',
+          },
+        },
+      );
+    } else {
+      Locations.update(
+        { _id: locationId },
+        {
+          $set: {
+            checkedInUserId: null,
+          },
+        },
+      );
+    }
   },
 });
